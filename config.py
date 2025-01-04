f57042bfac3f68966b61cbe5784b5701d10cc200
ANISH <153320703+codex-ML@users.noreply.github.com>
2025-01-04 23:02:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = 22108239
+API_HASH = "72b103fb198143c9b8e739784eb5d557"
+BOT_TOKEN = "6417942588:AAEE86o9kKFj6nt4O10ZQqwnWJT_Y8MvYWQ"
+MONGO_URI = "mongodb+srv://akg:akg8894@cluster0.ajoin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = "vehicle"
+CHANNEL = -1002138679081
